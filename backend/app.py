b49a416c9bad92243ed8bb64c84e62ef3383ce34
Varsha Thomas <varshathomas364@gmail.com>
2025-02-23 19:06:37 +0530

    Added MongoDB

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,22 @@
+import os
+from dotenv import load_dotenv
+from pymongo import MongoClient
+
+load_dotenv() # Load environment variables from .env file
+
+MONGO_URI=os.getenv("MONGO_URI") 
+
+client=MongoClient(MONGO_URI) # Create a new client instance
+db=client["phishing_sim"] # Select the database
+
 from flask import Flask
 from flask import render_template
 from flask import url_for
 
 app = Flask(__name__)
 
-@app.route('/hello/')
-@app.route('/hello/<name>')
+@app.route('/hello/') #Prints "Hello Stranger!"
+@app.route('/hello/<name>') #Prints "Hello <name>!"
 def hello(name=None):
     return render_template('hello.html', name=name)
 
