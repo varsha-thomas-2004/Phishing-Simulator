a503ac42adf9d512c8a7ab66f626dd4374789c1f
Varsha Thomas <varshathomas364@gmail.com>
2025-02-23 21:24:31 +0530

    Email collection successful

--- backend/app.py
+++ backend/app.py
@@ -4,24 +4,27 @@ from pymongo import MongoClient
 
 load_dotenv() # Load environment variables from .env file
 
-MONGO_URI=os.getenv("MONGO_URI") 
+MONGO_URI = os.getenv("MONGO_URI") 
 
-client=MongoClient(MONGO_URI) # Create a new client instance
-db=client["phishing_sim"] # Select the database
+client = MongoClient(MONGO_URI) # Create a new client instance
+db = client["phishing_sim"] # Select the database
 
-from flask import Flask
-from flask import render_template
-from flask import url_for
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/hello/') #Prints "Hello Stranger!"
-@app.route('/hello/<name>') #Prints "Hello <name>!"
-def hello(name=None):
-    return render_template('hello.html', name=name)
+@app.route('/log-click', methods=['GET','POST'])
+def log_click():
+    data = request.json
+    email = data.get("email")
+    collection = db["emails"]
+    if email:
+        collection.insert_one({"email" : email})
+        return jsonify({"message" : "Email collected successfully"}) , 201
+    else:
+        return jsonify({"error" : "Email is required"}), 400
 
-with app.test_request_context():
-    print(url_for('hello'))
+
 
 if __name__ == "__main__":
     app.run(debug=True)
